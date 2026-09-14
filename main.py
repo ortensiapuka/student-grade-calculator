53e06202e9701d31a7e62d963f266a2ebdb77093
ortensiapuka <k2523506@kingston.ac.uk>
2026-09-14 16:33:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+grade1 = int(input("Enter your first grade:"))
+grade2 = int(input("Enter your second grade:"))
+grade3 = int(input("Enter your third grade:"))
+average = (grade1 + grade2 + grade3) / 3
+print("Student:", name)
+print("Average grade:", average) 
